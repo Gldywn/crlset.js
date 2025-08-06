8dac889d8f8e1edb76d7e70edc29d341c1e0a964
Gldywn <14254051+Gldywn@users.noreply.github.com>
2025-08-06 17:06:49 +0200

    fix: update path resolution again

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -1,8 +1,9 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 /* istanbul ignore next */
 export function getDirname(): string {
   if (typeof import.meta?.url === 'string') {
-    const { fileURLToPath } = require('url');
-    const { dirname } = require('path');
     return dirname(fileURLToPath(import.meta.url));
   }
   return __dirname;
